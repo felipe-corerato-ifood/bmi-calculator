57c672f0e1ec49e78227a8e001ed929399822d06
FelipeCorerato <felipe.corerato@ifood.com.br>
2020-05-08 21:37:29 -0300

    feat(calculator): adds calculator class

---
+++ lib/core/calculator_brain.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import '../constants/constants.dart';
+
+class CalculatorBrain {
+  final Gender gender;
+  final int height;
+  final int weight;
+  final int age;
+
+  double _bmi;
+
+  CalculatorBrain(this.gender, this.height, this.weight, this.age);
+  
+  String calculateBMI() {
+    _bmi = weight / pow(height/100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'OVERWEIGHT';
+    } else if (_bmi > 18.5) {
+      return 'NORMAL';
+    } else {
+      return 'UNDERWEIGHT';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
+}
