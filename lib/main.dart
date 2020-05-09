62b11ce4df0336e62c1a3c6d26106d0f8d597bae
FelipeCorerato <felipe.corerato@ifood.com.br>
2020-05-08 21:39:51 -0300

    refactor(main): refacots main initial file

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'pages/input_page.dart';
+import 'constants/constants.dart';
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: kAppColors['primaryColor'],
+        scaffoldBackgroundColor: kAppColors['scaffoldBackgroundColor'],
+      ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
